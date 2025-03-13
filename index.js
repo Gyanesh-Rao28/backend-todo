132a0692ee6ff9966813d9ce5ce5c8796946a466
Gyanesh Rao <gyaneshrao28@gmail.com>
2025-03-13 22:18:49 +0530

    V-1.0

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://todo-chi-plum.vercel.app" || "http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"],
