34a62b28520c7d75f097b6f04da3494e0a0a56d1
Gyanesh Rao <gyaneshrao28@gmail.com>
2025-03-13 21:03:34 +0530

    V-1.0

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import User from "../model/user.model.js";
+import User from "../model/user.model";
 export const authenticate = async (req, res, next) => {
   const token = req.cookies.jwt;
   // console.log(token)
